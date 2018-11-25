283392028cae916427c1c15ecdbca61dbf395a67
Nathee <s_nathee@live.com>
2018-11-25 17:07:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 var config = {
-apiKey: "AIzaSyD0-A8dwJLXZVGo1a4Mjo4_0fxJl4JdQYc",
-authDomain: "storagewebdev04.firebaseapp.com",
-databaseURL: "https://storagewebdev04.firebaseio.com",
-projectId: "storagewebdev04",
-storageBucket: "storagewebdev04.appspot.com",
-messagingSenderId: "761240405099"
+    apiKey: "AIzaSyD0-A8dwJLXZVGo1a4Mjo4_0fxJl4JdQYc",
+    authDomain: "storagewebdev04.firebaseapp.com",
+    databaseURL: "https://storagewebdev04.firebaseio.com",
+    projectId: "storagewebdev04",
+    storageBucket: "storagewebdev04.appspot.com",
+    messagingSenderId: "761240405099"
 };
 firebase.initializeApp(config);
 
@@ -13,49 +13,97 @@ firebase.initializeApp(config);
 const preObject = document.getElementById('kk');
 //   Get elecnets
 const Btn01 = document.getElementById('btn01');
+const Btn02 = document.getElementById('btn02');
+const Btn03 = document.getElementById('btn03');
 // Create references
-const dbRefObject = firebase.database().ref().child('object');
-
-const newStoreRef = dbRefObject.push();
-
-// var newObject = newStoreRef.child('object').push();
+const dbRefObject01 = firebase.database().ref().child('/Arduino/OUTPUT01');
+const dbRefObject02 = firebase.database().ref().child('/Arduino/OUTPUT02');
+const dbRefObject03 = firebase.database().ref().child('/Arduino/OUTPUT03');
 
 const tt = document.getElementById('tt');
 const tt01 = document.getElementById('tt01');
-var msg = "",status ="",ttcode="";
+var msg = "", status = "", ttcode = "";
 
-(function() {
+(function () {
 
-// Sync object changes
-dbRefObject.on('value', snap => {
-    status = snap.val();
-    if(status == "ON"){
-        preObject.innerHTML = "กำลังทำงาน";
-    }else{
-        preObject.innerHTML = "พร้อมทำงาน";
-    }
+    // Sync object changes
+    dbRefObject01.on('value', snap => {
+        status = snap.val();
+        if (status == "ON") {
+            preObject.innerHTML = "กำลังทำงาน";
+        } else {
+            preObject.innerHTML = "พร้อมทำงาน";
+        }
 
-    if(ttcode=="01"){
-        // tt.innerHTML = "โต๊ะที่ 01";
-    }
-    
-    if(preObject.innerHTML=="พร้อมทำงาน"){
-        Btn01.classList.remove("deactive");
-        Btn01.classList.add("active");
-    }else{
-        Btn01.classList.add("deactive");
-        Btn01.classList.remove("active");
-    }
-});
+        if (preObject.innerHTML == "พร้อมทำงาน") {
+            Btn01.classList.remove("deactive");
+            Btn01.classList.add("active");
+        } else {
+            Btn01.classList.add("deactive");
+            Btn01.classList.remove("active");
+        }
+    });
+
+    dbRefObject02.on('value', snap => {
+        status = snap.val();
+        if (status == "ON") {
+            preObject.innerHTML = "กำลังทำงาน";
+        } else {
+            preObject.innerHTML = "พร้อมทำงาน";
+        }
+
+        if (preObject.innerHTML == "พร้อมทำงาน") {
+            Btn02.classList.remove("deactive");
+            Btn02.classList.add("active");
+        } else {
+            Btn02.classList.add("deactive");
+            Btn02.classList.remove("active");
+        }
+    });
+
+    dbRefObject03.on('value', snap => {
+        status = snap.val();
+        if (status == "ON") {
+            preObject.innerHTML = "กำลังทำงาน";
+        } else {
+            preObject.innerHTML = "พร้อมทำงาน";
+        }
+
+        if (preObject.innerHTML == "พร้อมทำงาน") {
+            Btn03.classList.remove("deactive");
+            Btn03.classList.add("active");
+        } else {
+            Btn03.classList.add("deactive");
+            Btn03.classList.remove("active");
+        }
+    });
 
 }());
 
-function toggleEvent01(){
-    ttcode ="01";
-    if(preObject.innerHTML=="พร้อมทำงาน"){
+function toggleEvent01() {
+    ttcode = "01";
+    if (preObject.innerHTML == "พร้อมทำงาน") {
         msg = "ON";
-    }else{
+    } else {
         msg = "OFF";
     }
-    dbRefObject.set(msg);    
+    dbRefObject01.set(msg);
+}
+
+function toggleEvent02() {
+    if (preObject.innerHTML == "พร้อมทำงาน") {
+        msg = "ON";
+    } else {
+        msg = "OFF";
+    }
+    dbRefObject02.set(msg);
+}
+
+function toggleEvent03() {
+    if (preObject.innerHTML == "พร้อมทำงาน") {
+        msg = "ON";
+    } else {
+        msg = "OFF";
+    }
+    dbRefObject03.set(msg);
 }
